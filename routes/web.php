e7f820ac33302f4068e3ad6ebe9ab1ad27156dbb
Devpph <52081212+thomaswinchestar@users.noreply.github.com>
2020-07-04 16:13:31 +0630

    second commit

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,9 @@ Route::get('/', 'ArticleController@index');
 Route::get('/articles', 'ArticleController@index');
 Route::get('/articles/detail/{id}', 'ArticleController@detail');
 Route::get('/products', 'Product/ProductController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/articles/add', 'ArticleController@add');
+Route::post('/articles/add', 'ArticleController@create');
+Route::get('/articles/delete/{id}', 'ArticleController@delete');
